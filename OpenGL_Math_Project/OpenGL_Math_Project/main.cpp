33ad66d9b93547fc7b00d1f61ffa75c2096e5b3a
Epono <guillaume.ambrois@gmail.com>
2016-02-13 16:56:58 +0100

    Mabite final ?

--- OpenGL_Math_Project/OpenGL_Math_Project/main.cpp
+++ OpenGL_Math_Project/OpenGL_Math_Project/main.cpp
@@ -131,12 +131,26 @@ void InitCubemap()
 	};
 
 	static const char* skyboxFiles[] = {
-		"skybox/right.jpg",
-		"skybox/left.jpg",
-		"skybox/top.jpg",
-		"skybox/bottom.jpg",
-		"skybox/back.jpg",
-		"skybox/front.jpg",
+		//"skybox/right.jpg",
+		//"skybox/left.jpg",
+		//"skybox/top.jpg",
+		//"skybox/bottom.jpg",
+		//"skybox/back.jpg",
+		//"skybox/front.jpg",
+
+		//"skybox/Powerlines/posx.jpg",
+		//"skybox/Powerlines/negx.jpg",
+		//"skybox/Powerlines/posy.jpg",
+		//"skybox/Powerlines/negy.jpg",
+		//"skybox/Powerlines/posz.jpg",
+		//"skybox/Powerlines/negz.jpg",
+
+		"skybox/Park3/posx.jpg",
+		"skybox/Park3/negx.jpg",
+		"skybox/Park3/posy.jpg",
+		"skybox/Park3/negy.jpg",
+		"skybox/Park3/posz.jpg",
+		"skybox/Park3/negz.jpg",
 	};
 
 	LoadAndCreateCubeMap(skyboxFiles, g_CubeMap.textureObj);
